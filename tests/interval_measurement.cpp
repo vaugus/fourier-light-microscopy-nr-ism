abc78f1018a72918ad3c3baf18b00d513a71031e
Victor Augusto Alves Catanante <vaugusto@alumni.usp.br>
2026-06-16 21:39:55 +0200

    refactor: remove unused test files and folders

--- tests/interval_measurement.cpp
+++
@@ -1,7 +0,0 @@
-auto start = chrono::steady_clock::now();
-
-auto end = chrono::steady_clock::now();
-
-cout << "Elapsed time in nanoseconds : " 
-    << chrono::duration_cast<chrono::nanoseconds>(end - start).count()
-    << " ns" << endl;
