673bebe0f35a7ff8906146d27cf547acfa730089
Victor Augusto Alves Catanante <vaugusto@alumni.usp.br>
2026-06-22 22:24:12 +0200

    feat: entrypoint error handling and command line params

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,33 @@
 #include "../lib/include/sample_function.hpp"
 #include "../lib/include/fft_utils.hpp"
 #include "../lib/include/helper.hpp"
+#include <memory>
 #include <iostream>
-#include <ctime>
-
-using namespace std;
 
 int main(int argc, char **argv) {
-    std::vector<cv::Mat> img_vec;
-    std::string const path = argv[1];
+    if (argc != 3)
+    {
+        std::cerr << "Usage: " << argv[0] << " <input_image_path> <output_path>" << std::endl;
+        return 1;
+    }
+    
+    try {
+        std::vector<cv::Mat> img_vec;
+        std::string const path = argv[1];
 
-    SampleFunction *sample_function = new SampleFunction();
-    Helper *helper = new Helper();
+        std::unique_ptr<SampleFunction> sample_processor = std::make_unique<SampleFunction>();
+        std::unique_ptr<Helper> helper_processor = std::make_unique<Helper>();
 
-    helper->open_dataset(img_vec, path);
+        helper_processor->open_dataset(img_vec, path);
+        sample_processor->run_batch_fft(img_vec);
+        img_vec.clear();
+    } catch (const std::exception& e) {
+        std::cerr << "An exception occurred during execution: " << e.what() << std::endl;
+        return 1;
+    } catch (...) {
+        std::cerr << "An unknown error occurred." << std::endl;
+        return 1;
+    }
 
-    sample_function->run_batch_fft(img_vec);
-
-    delete sample_function;
-    delete helper;
-    img_vec.clear();
-
-    return 0; 
+    return 0;
 }
