77dfa898ed76ad6a7c30d227462e4a18c1596012
Victor Augusto Alves Catanante <vaugusto@alumni.usp.br>
2026-07-04 23:30:19 +0200

    refactor: finish changing variables to camelCase

--- src/ftiqa/sample_function.cpp
+++ src/ftiqa/sample_function.cpp
@@ -19,12 +19,12 @@ using namespace std;
 /**
  * Parametrizes a batch FFT approach execution.
  *
- * @param img_vec      The image to suffer the analysis.
+ * @param images    The image to suffer the analysis.
  * @param params    The image to suffer the analysis.
  */
-void SampleFunction::runBatchFFT(std::vector<cv::Mat> &img_vec) {
+void SampleFunction::runBatchFFT(std::vector<cv::Mat> &images) {
     // Generate the radial vectors for all spectra
-    const unsigned n = getFinalSpectrumSize(img_vec[0]);
+    const unsigned n = getFinalSpectrumSize(images[0]);
     SampleFunctionBase::generateRadialVectors(n);
 
     StatisticalAnalysis *analysis = new StatisticalAnalysis();
@@ -33,37 +33,37 @@ void SampleFunction::runBatchFFT(std::vector<cv::Mat> &img_vec) {
     writer->setDelimiter(",");
     writer->setLineCount(0);
 
-    cv::Mat gray_spectrum;
+    cv::Mat graySpectrum;
     std::vector<std::vector<double>> coefficients;
     std::vector<double> tmp;
 
     std::vector<cv::Mat>::iterator it;
-    for (it = std::begin(img_vec); it != std::end(img_vec); ++it) {
+    for (it = std::begin(images); it != std::end(images); ++it) {
         // perform the fast fourier transform
-        SampleFunctionBase::fft(*it, gray_spectrum);
+        SampleFunctionBase::fft(*it, graySpectrum);
 
         // extract the coefficient vector
-        tmp = SampleFunctionBase::computeSampleFunction(gray_spectrum);
+        tmp = SampleFunctionBase::computeSampleFunction(graySpectrum);
 
         coefficients.emplace_back(tmp);
 
-        gray_spectrum.release();
+        graySpectrum.release();
         tmp.clear();
     }
 
     std::vector<std::vector<double>> data;
-    std::vector<std::vector<double>> kurtosis_array;
+    std::vector<std::vector<double>> kurtosisArray;
 
-    analysis->computeKurtosisAllCropSizes(coefficients, data, kurtosis_array);
+    analysis->computeKurtosisAllCropSizes(coefficients, data, kurtosisArray);
     
-    const unsigned crop = analysis->findMaximumRange(kurtosis_array);
+    const unsigned crop = analysis->findMaximumRange(kurtosisArray);
     
     analysis->computeDatasetIqr(data, crop);
     
     writer->setFilename("output/descriptor/dataset.csv");
     writer->writeFFTDescriptorDataset(coefficients);
 
-    img_vec.clear();
+    images.clear();
     delete writer;
 }
 
